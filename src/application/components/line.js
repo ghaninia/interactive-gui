534c39d9762dffb9508d031d8615d6026f778865
Amin Ghaninia <mohamadaminghaninia@gmail.com>
2022-10-28 14:48:04 +0330

    feature : proccess arrow button

--- src/application/components/line.js
+++ src/application/components/line.js
@@ -8,101 +8,144 @@ const Line = React.forwardRef ( (props, ref) => {
     const [ queries , setQueries ] = React.useState([]) ;
     const [ sentence  , setSentence ]  = React.useState("");
     const [ loading , setLoading ] = React.useState(false) ;
+    const [ pointer , setPointer ] = React.useState(0) ;
+
+    React.useEffect(() => {
+        setLoading(false) ;
+        setPointer( queries.length ) ;
+        emptyCurrentSentence() ;
+    },[queries]) ;
 
     /**
-     * request api 
+     * preccess sentence 
      */
-    const request = async ( uri ) => {
+    const request = async ( event , uri ) => {
 
-        /**
-         * validation err 
-         */
-        if(! sentenceIsValid()) {
-            setLoading(false);
-            return emptyCurrentSentence() ;
+        switch ( sentence )
+        {
+            case "" : {
+                setLoading(false) ;
+                event.preventDefault() ;
+                break;
+            }
+            case "exit":
+            case "cls" :
+            case "clear" : {
+                setLoading(false) ;
+                setQueries([]) ;
+                emptyCurrentSentence() ;
+
+                break;
+            }
+            default : {
+                /**
+                 * when has value 
+                 */
+                await (new Request(uri , { sentence }))
+                    .post()
+                    .then(function(response){
+                        setQueries([
+                            ... queries , 
+                            {
+                                query : sentence ,
+                                result : response?.result
+                            }
+                        ] );
+                    })
+                    .catch(function(error){
+                        setQueries([
+                            ... queries , 
+                            {
+                                query : sentence ,
+                                message : error?.message ,
+                                error : error?.code
+                            }
+                        ]);
+                    });
+                break ;
+            }
         }
 
-        /**
-         * api request
-         */
-        await (new Request(uri , { sentence }))
-            .post()
-            .then(function(response){
-                setQueries([
-                    ... queries , 
-                    {
-                        query : sentence ,
-                        result : response?.result
-                    }
-                ] );
-                emptyCurrentSentence() ;
-            })
-            .catch(function(error){
-                setQueries([
-                    ... queries , 
-                    {
-                        query : sentence ,
-                        message : error?.message ,
-                        error : error?.code
-                    }
-                ]);
-                emptyCurrentSentence() ;
-            });
     } ;
 
     
     /**
-     * 
+     * when user clicked button 
      */
     const handleKeyDown = (event , data) => {
         switch (event.key) {
+            /**
+             * clicked enter button 
+             */
             case 'Enter': {
-
                 if( ! loading ) 
                 {
                     setLoading(true) ;
-                    request(data?.uri?.set);
+                    request(event ,data?.uri?.set);
+                } else {
+                    event.preventDefault() ;
                 }
-
                 break;
             }
-            default:
+            /**
+             * clicked arrow up button 
+             */
+            case "ArrowUp" : {
+                var currentPointer = pointer - 1 ;
+                if( currentPointer < 0 ) return ;
+                setPointer(currentPointer) ;
+                var currentQuery = queries[ currentPointer ] ;
+                changeFieldTo(currentQuery.query) ;
                 break;
+            }
+            /**
+             * clicked arrow down button 
+             */
+            case "ArrowDown" : {
+                var currentPointer = pointer + 1 ;
+
+                if( currentPointer > queries.length ) {
+
+                } else if (currentPointer == queries.length) {
+                    setPointer(currentPointer) ;
+                } else {
+                    setPointer(currentPointer) ;
+                    var currentQuery = queries[ currentPointer ] ;
+                    changeFieldTo(currentQuery.query) ;
+                }
+                break;
+            }
+            /**
+             * press other button
+             */
+            default: {
+                setSentence(event.currentTarget.textContent);
+            }
         }
     }
 
     /**
-     * change input 
+     * change field state to new thing 
      */
-     const handleOnInput = event => {
-        setSentence(event.currentTarget.textContent);
-    };
+    const changeFieldTo = (newSentence) => {
+        var field = ref.current ;
+        if( field ) 
+        {
+            field.innerText = newSentence ;
+            setSentence(newSentence) ;
+        }
+
+    }
 
     /**
-     * 
+     * empty terminal line 
      */
     const emptyCurrentSentence = () => {
-        var field = ref.current ;
-        if(field) {
-            field.innerText = "" ;
-            setSentence("") ;
-        }
-    }
-
-    /** 
-     * 
-     */
-    const sentenceIsValid = () => {
-        if( sentence === "")
-        {
-            alert("You should fill command line!")
-            return false ;
-        }
-        return true ;
+        changeFieldTo("") ;
     }
 
     /**
-     * 
+     * render HTML 
      */
     return (
         <DataContext.Consumer>
@@ -119,8 +162,7 @@ const Line = React.forwardRef ( (props, ref) => {
                                 suppressContentEditableWarning={true}
                                 className="user-input" 
                                 ref={ ref } 
-                                onInput={e =>  handleOnInput(e) }
-                                onKeyDown={ e => handleKeyDown(e , data) } ></span>
+                                onKeyUp={ e => handleKeyDown(e , data) } ></span>
                         </div>
                     </>     
                 )
